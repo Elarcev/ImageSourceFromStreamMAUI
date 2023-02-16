82244b4a3ea7dc9524d9f8eb554551de33af2b3b
Elarcev <60932276+Elarcev@users.noreply.github.com>
2023-02-16 17:36:06 +0700

    Create ImageSourceInModel.cs

---
+++ ImageSourceInModel.cs
@@ -0,0 +1,29 @@
+ //Converting base64 data to image
+        public ImageSource CustomerImage
+        {
+            get
+            {
+                try
+                {
+                    if (HasImage)
+                    {
+                        byte[] imageBytes = Convert.FromBase64String(img_data);
+                        _imageSource = ImageSource.FromStream(() => new MemoryStream(Convert.FromBase64String(img_data)));
+                        return _imageSource;
+                    }
+                    else
+                    {
+                        return _imageSource = null;
+                    }
+                }
+                catch
+                {
+                    return _imageSource = null;
+                }
+            }
+            set
+            {
+                _imageSource = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CustomerImage)));
+            }
+        }
