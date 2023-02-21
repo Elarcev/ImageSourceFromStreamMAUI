4e8b3a53199e9e76d6df73262116bf3222011aa4
Elarcev <60932276+Elarcev@users.noreply.github.com>
2023-02-21 11:20:46 +0700

    Full Issue View Project

---
+++ ImageSourceIssue/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ImageSourceIssue.ViewModel;
+
+namespace ImageSourceIssue;
+
+public partial class MainPage : ContentPage
+{
+    private readonly MainViewModel ViewModel;
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+        ViewModel = vm;
+        BindingContext = ViewModel;
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        ViewModel.TestImage = ImageSource.FromFile("dotnet_bot.png");
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        ViewModel.TestImage = ImageSource.FromStream(() => new MemoryStream(Convert.FromBase64String(ViewModel.base64image)));
+    }
+}
+
